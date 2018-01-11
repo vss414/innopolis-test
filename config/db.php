604949f518609422aca1b3a6308f4ba90632eb1c
s.vozhov <s.vozhov@softomate.com>
2018-01-11 11:57:25 +0700

    add migration

--- config/db.php
+++ config/db.php
@@ -2,10 +2,15 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
+    'dsn' => 'pgsql:host=localhost;port=5432',
+    'username' => 'postgres',
     'password' => '',
     'charset' => 'utf8',
+    'schemaMap' => [
+        'pgsql'=> [
+            'class'=>'yii\db\pgsql\Schema',
+        ]
+    ]
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
---
+++ migrations/m180111_035029_add_tables.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180111_035029_add_tables
+ */
+class m180111_035029_add_tables extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->createTable('resume', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(255),
+            'description' => $this->text()
+        ]);
+        $this->createTable('competence', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(255),
+        ]);
+        $this->createIndex('unique_competence', 'competence', 'title', true);
+        $this->createTable('resume_competence', [
+            'id' => $this->primaryKey(),
+            'resume_id' => $this->integer(),
+            'competence_id' => $this->integer(),
+        ]);
+        $this->addForeignKey(
+            'fk_rc_resume_id',
+            'resume_competence',
+            'resume_id',
+            'resume',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+        $this->addForeignKey(
+            'fk_rc_competence_id',
+            'resume_competence',
+            'competence_id',
+            'competence',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropTable('resume_competence');
+        $this->dropTable('competence');
+        $this->dropTable('resume');
+    }
+}
